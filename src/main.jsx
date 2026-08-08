c3fdd1f3d67d217f0a51d43504b846fbe324f9e2
Pramod Behera <118267946+PramodStack@users.noreply.github.com>
2026-08-08 20:16:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -6,12 +7,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import cartReducer from "./CartSlice";
+
 import "./App.css";
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
+    cart: cartReducer,
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
