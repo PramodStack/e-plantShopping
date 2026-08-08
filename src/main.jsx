a7bf8fc3e8abe36edf58bb19d5ff701787172558
Pramod Behera <118267946+PramodStack@users.noreply.github.com>
2026-08-08 19:53:23 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import cartReducer from "./CartSlice";
+import "./App.css";
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer
+  }
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
